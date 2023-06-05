b2d95dff3da9b7b6526ad763ff84dde1bff97a1b
Luis Victor <Luisvictor202222@gmail.com>
2023-06-05 12:47:14 -0300

    condições

---
+++ aula03/ex001.js
@@ -0,0 +1,6 @@
+var vel = 60
+console.log(`A velocidade do seu carro é ${vel} Km/hr`)
+if (vel > 58 ){ //Condição simples por haver apenas uma condição 'if -->Se'
+    console.log(`Voçê ultrapassou a velocidade de partida. Multado!`)
+}
+console.log(`Dirija sempre usando cinto de segurança!`)
---
+++ aula03/ex002.js
@@ -0,0 +1,7 @@
+var país = "Eua"
+console.log(`Vivendo em ${país}`)
+    if (país != "brasil"){
+console.log(`Voçê é estrangeiro`)}
+ else{
+    console.log(`Voçê é  Brasileiro`)
+}
