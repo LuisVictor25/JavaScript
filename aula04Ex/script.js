168e23ef83c4a109fe7cd47f182bee67044ba045
Luis Victor <Luisvictor202222@gmail.com>
2023-07-06 14:17:45 -0300

    atualizando

--- aula04Ex/script.js
+++ aula04Ex/script.js
@@ -1,18 +1,19 @@
 function carregar(){
 
-    var msg = window.document.getElementById("msg")
-    var foto = window.document.getElementsByTagName("foto")
+    var imsg = window.document.getElementById("imsg")
+    var img = window.document.getElementById("imagem")
     var data = new Date()
     var hora = data.getHours()
-    msg.innerHTML = `Agora são ${hora}horas`
+    imsg.innerHTML = `Agora são ${hora}horas`
     if(hora>= 0 && hora < 12){
         img.src ='Manhã.png'
         document.body.style.background = "#f4dab9"
     }else if(hora >= 12 && hora <= 18){
         img.src ='FimdeTarde.png'
         
-    }else
+    }else 
         img.src = 'Noite.png'
         document.body.style.background = "#f4dab9"
-    }
+}
+
 
