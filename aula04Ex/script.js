9917ea907012971243b09b25bf5eb797c97990de
Luis Victor <Luisvictor202222@gmail.com>
2023-07-06 10:53:13 -0300

    função hora

--- aula04Ex/script.js
+++ aula04Ex/script.js
@@ -1,1 +1,18 @@
-alert('Olá!')
+function carregar(){
+
+    var msg = window.document.getElementById("msg")
+    var foto = window.document.getElementsByTagName("foto")
+    var data = new Date()
+    var hora = data.getHours()
+    msg.innerHTML = `Agora são ${hora}horas`
+    if(hora>= 0 && hora < 12){
+        img.src ='Manhã.png'
+        document.body.style.background = "#f4dab9"
+    }else if(hora >= 12 && hora <= 18){
+        img.src ='FimdeTarde.png'
+        
+    }else
+        img.src = 'Noite.png'
+        document.body.style.background = "#f4dab9"
+    }
+
